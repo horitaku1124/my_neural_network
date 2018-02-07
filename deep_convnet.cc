2a7779be6dd27c06057b24a4165cde7815c8cce6
Takuya Horimatsu <new_woory@hotmail.com>
2018-02-08 01:59:58 +0900

    param

--- deep_convnet.cc
+++ deep_convnet.cc
@@ -5,6 +5,18 @@ struct FilterParam {
     int pad;
     int stride;
 };
+
+struct ParamsArray {
+    MyNum W1, b1;
+    MyNum W2, b2;
+    MyNum W3, b3;
+    MyNum W4, b4;
+    MyNum W5, b5;
+    MyNum W6, b6;
+    MyNum W7, b7;
+    MyNum W8, b8;
+};
+
 struct DeepConvNet {
     int *input_dim;
     FilterParam param1,
@@ -32,9 +44,30 @@ struct DeepConvNet {
         hidden_size = 50;
         output_size = 10;
 
-        MyNum pre_node_nums = MyNum(8, 1*3*3, 16*3*3, 16*3*3, 32*3*3, 32*3*3, 64*3*3, 64*4*4, hidden_size);
-        pre_node_nums.divideOf(2.0);
-        pre_node_nums.sqrt();
+        MyNum wight_init_scales = MyNum(8, 1*3*3, 16*3*3, 16*3*3, 32*3*3, 32*3*3, 64*3*3, 64*4*4, hidden_size);
+        wight_init_scales.divideOf(2.0);
+        wight_init_scales.sqrt();
         
+        int pre_channel_num = _input_dim[0];
+
+        ParamsArray params = ParamsArray();
+        params.W1 = randome_randn(4, _param1.num, pre_channel_num, _param1.size, _param1.size) * wight_init_scales.getInt(0);
+        params.b1 = zeros(_param1.num);
+
+        params.W2 = randome_randn(4, _param2.num, pre_channel_num, _param2.size, _param2.size) * wight_init_scales.getInt(1);
+        params.b2 = zeros(_param2.num);
+
+        params.W3 = randome_randn(4, _param3.num, pre_channel_num, _param3.size, _param3.size) * wight_init_scales.getInt(2);
+        params.b3 = zeros(_param3.num);
+
+        params.W4 = randome_randn(4, _param4.num, pre_channel_num, _param4.size, _param4.size) * wight_init_scales.getInt(3);
+        params.b4 = zeros(_param4.num);
+
+        params.W5 = randome_randn(4, _param5.num, pre_channel_num, _param5.size, _param5.size) * wight_init_scales.getInt(4);
+        params.b5 = zeros(_param5.num);
+
+        params.W6 = randome_randn(4, _param6.num, pre_channel_num, _param6.size, _param6.size) * wight_init_scales.getInt(5);
+        params.b6 = zeros(_param6.num);
+
     }
 };
--- my_num.cc
+++ my_num.cc
@@ -1,17 +1,25 @@
+#define TYPE_INT 1;
+#define TYPE_FLOAT 2;
+
+
 struct MyNum {
     size_t _size;
+    size_t _dimension;
     short _type;
     int *_int_array;
-    double *double_array;
+    float *_float_array;
+    double *_double_array;
     MyNum() {
         _size = 0;
+        _dimension = 0;
         _int_array = NULL;
     }
     MyNum(int count, ...) {
         va_list ap;
         va_start(ap, count);
         _int_array = (int *)malloc(sizeof(int) * count);
-        _type = 1;
+        _type = TYPE_INT;
+        _dimension = 1;
         
         for (int i = 0; i < count; i++ ) {
             _int_array[i] = va_arg(ap, int);
@@ -28,20 +36,76 @@ struct MyNum {
     }
 
     void divideOf(float n) {
-        double_array = (double *)malloc(sizeof(double) * _size);
+        _double_array = (double *)malloc(sizeof(double) * _size);
         for (int i = 0;i < _size;i++) {
-            double_array[i] = n / _int_array[i];
+            _double_array[i] = n / _int_array[i];
         }
         free(_int_array);
     }
     void sqrt() {
         for (int i = 0;i < _size;i++) {
-            double_array[i] = ::sqrt(double_array[i]);
-        }
-        for (int i = 0;i < _size;i++) {
-            printf("[%d]=%f\n", i, double_array[i]);
+            _double_array[i] = ::sqrt(_double_array[i]);
         }
     }
     // ~MyNum(void) {
     // }
+
+    int getInt(size_t index) {
+        return _int_array[index];
+    }
+    MyNum operator*(int x) {
+        MyNum _res = MyNum();
+        _res._size = _size;
+        _res._dimension = _dimension;
+        _res._type = _type;
+        int t_int = TYPE_INT;
+        int t_float = TYPE_FLOAT;
+        if (_type == t_int) {
+
+        } else if (_type == t_float) {
+            _res._float_array = _float_array;
+            for (size_t i = 0;i < _size;i++) {
+                _res._float_array[i] *= x;
+            }
+        }
+        return _res;
+    }
 };
+MyNum randome_randn(int count, ...) {
+    va_list ap;
+    va_start(ap, count);
+
+    MyNum data;
+
+    int size = 1;
+    for (int i = 0; i < count; i++ ) {
+        size *= va_arg(ap, int);
+    }
+    va_end(ap);
+    data._size = size;
+    data._type = TYPE_FLOAT;
+    data._float_array = (float *)malloc(sizeof(float) * size);
+    for (size_t i = 0;i < size;i++) {
+        data._float_array[i] = (float)rand() / RAND_MAX;
+    }
+    return data;
+}
+MyNum zeros(int count, ...) {
+    va_list ap;
+    va_start(ap, count);
+
+    MyNum data;
+
+    int size = 1;
+    for (int i = 0; i < count; i++ ) {
+        size *= va_arg(ap, int);
+    }
+    va_end(ap);
+    data._size = size;
+    data._type = TYPE_INT;
+    data._int_array = (int *)malloc(sizeof(int) * size);
+    for (size_t i = 0;i < size;i++) {
+        data._int_array[i] = 0;
+    }
+    return data;
+}
